5ebdf95a3bdf1762def6786b99bbb11d8140fba4
fsdFelipe <devfsdfelipe@gmail.com>
2022-10-29 11:35:45 -0300

    Projeto Criado - Conexão com o BD

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+const app = express()
+dotenv.config()
+
+const connect =()=>{
+    mongoose.connect(process.env.MONGO).then(()=>{
+        console.log('Conectado ao MongoDB cloud')
+    }).catch((erro)=>{
+        throw erro
+    })
+}
+
+app.listen(3000, ()=>{
+    console.log("Server iniciado na porta 3000")
+    connect()
+})
